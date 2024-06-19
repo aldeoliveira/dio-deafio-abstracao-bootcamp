ae5a90f64eb61960b57dfd5bce24a69854d3165c
André <aldeoliveira@gmail.com>
2024-06-19 19:31:21 -0300

    adicionar classes Bootcamp e Dev

---
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -0,0 +1,44 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Bootcamp {
+    private String nome;
+    private String descricao;
+    private final LocalDate dataInicial;
+    private final LocalDate dataFinal;
+    private Set<Dev> devsInscritos = new HashSet<>();
+    private Set<Conteudo> conteudos = new LinkedHashSet<>();
+
+    public Bootcamp() {
+        this.dataInicial = LocalDate.now();
+        this.dataFinal = dataInicial.plusDays(45);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataInicial() {
+        return dataInicial;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+}
